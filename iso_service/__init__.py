a9b767409b7b2b71e5c4b19767722e966ead5649
Tomas Patro <tomas.patro@gmail.com>
2021-08-08 21:09:09 +0200

    Adjust env vars config

--- iso_service/__init__.py
+++ iso_service/__init__.py
@@ -6,18 +6,12 @@ from iso_service.blueprints import country
 
 
 def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(SECRET_KEY='dev')
+    app = Flask(__name__)
 
     if test_config is not None:
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('config.py')
     else:
         app.config.from_mapping(test_config)
-
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
     app.register_blueprint(country.bp)
 
