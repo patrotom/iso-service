6d1cf28ae3bad0a043360a0d6ae502e945570df4
Tomas Patro <tomas.patro@gmail.com>
2021-08-08 21:03:16 +0200

    Move country BP to blueprints dir

--- iso_service/__init__.py
+++ iso_service/__init__.py
@@ -2,14 +2,14 @@ import os
 
 from flask import Flask
 
-from iso_service import country
+from iso_service.blueprints import country
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='dev')
 
-    if test_config is None:
+    if test_config is not None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
