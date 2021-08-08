4df6a0937a25d53b81072bcdcc5a2945eece0ea8
Tomas Patro <tomas.patro@gmail.com>
2021-08-08 22:50:53 +0200

    Add Redis setup to GH workflows

--- iso_service/__init__.py
+++ iso_service/__init__.py
@@ -9,9 +9,9 @@ from iso_service.cache import cache
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    if test_config is None:
-        app.config.from_pyfile('config.py')
-    else:
+    app.config.from_pyfile('config.py')
+
+    if test_config is not None:
         app.config.from_mapping(test_config)
 
     cache.init_app(app)
